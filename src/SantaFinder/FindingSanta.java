91b01b9c02b83d6e5ec47772e2b01cb12e36f0fb
EdwardDSalazar <esalazar11@islander.tamucc.edu>
2020-12-24 12:16:26 -0600

    Code we wrote in class

--- src/SantaFinder/FindingSanta.java
+++ src/SantaFinder/FindingSanta.java
@@ -1,9 +1,33 @@
 package SantaFinder;
 
+import java.util.Scanner;
+
 public class FindingSanta {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		SantaSuspects santaSuspects = new SantaSuspects(6);
+		
+		santaSuspects.addSuspect("Bob Guy");
+		santaSuspects.addSuspect("Sally Girl");
+		santaSuspects.addSuspect("Petter Dude");
+		santaSuspects.addSuspect("John Boy");
+		santaSuspects.addSuspect("Pat Back");
+		santaSuspects.addSuspect("Dev Man");
+		
+		System.out.println("Provide a name to search for: ");
+		String searchFor = scanner.nextLine();
+		
+		boolean foundMatch = santaSuspects.foundMatch(searchFor);
+		if(foundMatch) {
+			System.out.println(searchFor + " is indeed a santa suspect");
+		}
+		else {
+			System.out.println(searchFor + " is not a santa suspect");
+		}
+		
 	}
 
 }
